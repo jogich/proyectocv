dbe40594cd6995fb5e8609d324af03e9b5434cdd
2daw <2daw@WINMAC11.alumnado.flo>
2017-09-20 09:15:20 +0200

    template login

---
+++ raiz/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <h2>INICAR SESIÓN</h2>
+    <form action="post">
+        Correo electrónico <input type="text" name="email" placeholder="Introduce tu correo electrónico..."><br><br>
+        Contraseña <input type="text" name="password" placeholder="Introduce tu contraseña..."><br><br>
+        <input type="submit" name="Entrar" value="Entrar">
+    </form>
+</body>
+</html>

1baa37569d471baeb94353b20ac8ec1623cd9675
2daw <2daw@WINMAC11.alumnado.flo>
2017-09-20 12:45:55 +0200

    removed login folder

--- raiz/login.php
+++ raiz/login.php
@@ -3,46 +3,63 @@
   $seguridad = new seguridad();
 ?>
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Iniciar sesión</title>
-</head>
-<body>
-    <h2>INICAR SESIÓN</h2>
-    <form method="post" action="login.php">
-        Correo electrónico <input type="text" name="email" placeholder="Introduce tu correo electrónico..."><br><br>
-        Contraseña <input type="password" name="password" placeholder="Introduce tu contraseña..."><br><br>
-        <input type="submit" name="Entrar" value="Entrar">
-    </form>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/login.css">
+  </head>
+  <body>
 
-    <?php
-        if ((isset($_POST['email'])) && (!empty($_POST['email'])) &&
-            (isset($_POST['password'])) && (!empty($_POST['password']))){
-                
-                include '../lib/usuario.php';
+    <div class="wrapper">
+    <div class="container">
+        <h1>Bienvenido</h1>
 
-                $seguridad = new seguridad();
+        <form class="form" method="post" action="login.php">
+            <input type="text" name="email" placeholder="Correo electrónico">
+            <input type="password" name="password" placeholder="Contraseña">
+            <button type="submit" id="login-button">Login</button>
+        </form>
+    </div>
 
-                $login = new Usuario();
-                
-                $resultado = $login->buscarUsuario($_POST['email']);
-                
-                if ($resultado['correo']==($_POST['email'])){
+    <ul class="bg-bubbles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
+<?php
+    if ((isset($_POST['email'])) && (!empty($_POST['email'])) &&
+        (isset($_POST['password'])) && (!empty($_POST['password']))){
+            
+            include '../lib/usuario.php';
 
-                    if ($resultado['contrasena']==(sha1($_POST['password']))){
-                        echo "Has iniciado sesión correctamente!<br><br>";
-                        echo "<h2>Bienvenido</h2>";
-                        
-                        $seguridad->addUsuario($_POST['email']);
-                    }
-                }else{
-                    echo "Este correo electrónico no existe. Registrese aquí.<br><br>";
-                    echo "<a href=registro.php>Registro</a>";
+            $seguridad = new seguridad();
+
+            $login = new Usuario();
+            
+            $resultado = $login->buscarUsuario($_POST['email']);
+            
+            if ($resultado['correo']==($_POST['email'])){
+
+                if ($resultado['contrasena']==(sha1($_POST['password']))){
+                    echo "Has iniciado sesión correctamente!<br><br>";
+                    echo "<h2>Bienvenido</h2>";
+                    
+                    $seguridad->addUsuario($_POST['email']);
                 }
-            } 
-    ?>
-</body>
+            }else{
+                echo "Este correo electrónico no existe. Registrese aquí.<br><br>";
+                echo "<a href=registro.php>Registro</a>";
+            }
+    } 
+?>
+  </body>
 </html>
